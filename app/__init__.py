73eb497e05c4a4b757cc9a10a77cf15bf5d3c495
Ji Hyuk Bae <jihyuk.b@gmail.com>
2017-01-20 22:27:37 -0500

    testing POST

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, abort, request
 
 app = Flask(__name__)
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,5 +1,15 @@
 from app import app
 
+@app.route('/api/submitreading', methods=['POST'])
+def create_task():
+    if not request.json or not 'temp' in request.json:
+        abort(400)
+    new_reading = {
+        'temp': request.json['temp']
+    }
+
+    return jsonify({'reading': new_reading}), 201
+
 @app.route('/')
 @app.route('/index')
 def index():
